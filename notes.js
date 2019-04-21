7642e0a8d79828cbe979469c67ea4fa4ef6ecd2f
lanners-marshall <lanners.marshall@gmail.com>
2019-04-21 16:39:42 -0700

    update

--- notes.js
+++ notes.js
@@ -125,8 +125,140 @@ for (let i in obj){
 
 console.log(obj)
 
+/*
+	In javascript functions have their own scope which is called functional scope. 
+	We also have the ability to nest functions as deeply as we want. 
+	Each time we nest a new function we create a new scope for that individual function. 
+*/
 
+//the problem with this example is that the number variable is set to a global scope. 
 
+let number = 0
+let upNumber = () => {
+  number = number + 1
+  return number
+}
+
+upNumber()
+upNumber()
+upNumber()
+console.log(number) // -> 3
+
+/*
+  for this example we have the number varaible set to the counter 
+  functions scope and its increase set to the nested functions scope. 
+  That way we can hold onto its value each time its called.
+*/
+
+let counter = () => {
+  let num = 0
+  return () => {
+    num = num + 1
+    return num
+  } 
+}
+
+let c = counter()
+console.log(c())
+console.log(c())
+console.log(c())
+console.log(c()) // -> 4
+
+/*
+  a callback function is when we pass a function to a javascript function as a parameter
+*/
+
+const multiply = (x, y) => {
+  return x * y
+}
+
+const add = (x, y) => {
+  return x + y
+}
+const subtract = (x, y) => {
+  return x - y
+}
+const divide = (x, y) => {
+  return x / y
+}
+
+const changeNumbers = (x, y, cb) => {
+  return cb(x, y)
+}
+
+console.log(changeNumbers(5, 5, multiply)) // -> 25
+console.log(changeNumbers(5, 5, add))// -> 10
+console.log(changeNumbers(5, 5, subtract)) // -> 0
+console.log(changeNumbers(5, 5, divide)) // -> 1
+
+/*
+	note arrow functions doe not have bindings to 
+	this, arguments, super, or new.target keywords
+*/
+
+//constructor function used to create objects
+
+function Person(obj){
+  this.name = obj.name
+  this.age = obj.age
+  this.greet = function(){
+    console.log(`hello my name is ${this.name}, I am ${this.age} years old`)
+  }
+}
+
+const marshall = new Person({name: 'Marshall', age: 36})
+marshall.greet()
+
+//example use of adding a method to the objects prototype
+
+function Fruit(attr){
+  this.name = attr.name
+  this.calories = attr.calories
+}
+
+Fruit.prototype.inspect = function(){
+  console.log(`the ${this.name} has ${this.calories} calories`)
+}
+
+const banana = new Fruit({name: 'banana', calories: '100'})
+
+banana.inspect()
+
+//with es6 we have classes which is just syntaxtic suger for the Prototypal pattern
+
+class Animal {
+  constructor(name){
+    this.name = name
+  }
+  speak(){
+    console.log(`${this.name} makes a sound.`)
+  }
+}
+
+let animal = new Animal('sam')
+animal.speak()
+
+class Dog extends Animal {
+  constructor(name){
+    super(name)
+  }
+  speak(){
+    console.log(`${this.name} barks!`)
+  }
+}
+
+let dog = new Dog('fred')
+dog.speak()
+
+class Lab extends Dog {
+  constructor(name, type){
+    super(name)
+    this.type = type
+  }
+}
+
+let lab = new Lab('sam', 'black lab')
+console.log(lab)
 
 
 
