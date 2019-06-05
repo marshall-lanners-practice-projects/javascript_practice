71f4961473a96f81bee4b79ba0bf5317759701e1
lanners-marshall <lanners.marshall@gmail.com>
2019-06-04 17:59:53 -0700

    update

---
+++ website/index.js
@@ -0,0 +1,9 @@
+let image 
+let start = document.querySelector('#a2')
+let end = document.querySelector('#a4')
+start.style.backgroundImage = "url('pawn.png')"
+let swap = () => {
+	start.style.backgroundImage = ''
+	end.style.backgroundImage = "url('pawn.png')"
+}
+
