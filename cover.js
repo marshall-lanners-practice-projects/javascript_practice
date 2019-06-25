d261e99d0e1907eb298ea8e8710c23c7686b7060
lanners-marshall <lanners.marshall@gmail.com>
2019-06-24 22:12:13 -0700

    update

--- cover.js
+++ cover.js
@@ -6,13 +6,15 @@ Marshall Lanners
 lanners.marshall@gmail.com
 mlanners.com
 
-Dear Joydrive Hiring Manager,
+Dear company Hiring Manager,
 
-I’m very interested in discussing the possibility of employment at Joydrive. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
+I’m very interested in discussing the possibility of employment at company. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
 
-I believe I would be a great fit for Joydrive because Joydrive needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project, I took ownership of developing the entire backend in Node, deploying both the backend and frontend, integrating the entire Api, developing several components with React and Redux, to developing the UI. I’m also a collaborator; I communicate effectively and efficiently, and, while I am confident in my abilities, I can recognize when I need to take a step back to ask for assistance from a senior developer. In addition, my experience has allowed me to prioritize my problem solving and critical thinking skills as I overcame increasingly complicated challenges.
+I like how you do this. I think that is good. I also like small detail on blog post. I thought small detail was neet.
 
-I would love to speak further about possible employment at Joydrive sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
+I believe I would be a great fit for company because company needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project, I took ownership of developing the entire backend in Node, deploying both the backend and frontend, integrating the entire Api, developing several components with React and Redux, to developing the UI. I’m also a collaborator; I communicate effectively and efficiently, and, while I am confident in my abilities, I can recognize when I need to take a step back to ask for assistance from a senior developer. In addition, my experience has allowed me to prioritize my problem solving and critical thinking skills as I overcame increasingly complicated challenges.
+
+I would love to speak further about possible employment at company sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
 Regards,
 
 Marshall Lanners
@@ -20,3 +22,7 @@ Marshall Lanners
 */
 
 console.log(this)
+
+import React from 'react';
+//import axios from 'axios';
+
--- website/trees.js
+++ website/trees.js
@@ -71,14 +71,50 @@
 						/\			 /\
 					12  30	110 130
 
-		what makes a balanced binary search tree balanced is if each node to the left
+		what makes a binary search tree is if each node to the left
 		of the root is less than the root and each node to the right is more than the root
 		and also for each sub root, as soon as its not it is no longer balanced
 
 		for example if I replace the 30 with 112 it would no longer be balanced because
 		the 112 is higher than the root value which is 100
 
+		what makes a tree balanced if is the difference between the hight of the left and right
+		tree sub tree is not greater than one
+
+
+		how do we create a balanced binary tree from a shorted imput array?
+
+		the pattern
+
+		1. the root is always the middle number
+		2. the root of the left sub tree is the middle of the left part of the sub array
+		3. the root of the right sub tree is the middle of the right part of the sub array
+
+		 0  1  2  3  4  5  6  // index's
+		[1, 2, 3, 4, 5, 6, 7] // array values
+
+		1. initialize start = 0, end is array.length - 1, mid = (start + end) / 2
+		2. create a tree node with mid as root (lets call it A).
+		3. Recursively do steps 4 and 5
+		4. Calculate mid of left subarray and make it root of left subtree of A
+		5. Calculate mid of right subarray and make it root of right subtree of A
+
+														4
+													/   \
+												 2		 6
+												/ \		/ \
+											 1   3 5   7
+
+		benefits of a binary search tree
+
+		it allows us to perform 
+
+		search in O(log n)
+		insert in O(log n)
+		remove in O(log n)
+
 		
+
 */
 
 
