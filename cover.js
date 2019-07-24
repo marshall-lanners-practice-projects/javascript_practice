ad54b70e8dc021914654e12e246c3c08ccb33439
lanners-marshall <lanners.marshall@gmail.com>
2019-07-24 14:51:38 -0700

    update

--- cover.js
+++ cover.js
@@ -5,15 +5,15 @@ Marshall Lanners
 lanners.marshall@gmail.com
 mlanners.com
 
-Dear Kambi Group plc Hiring Manager,
+Dear skillja Hiring Manager,
 
-I’m very interested in discussing the possibility of employment at Kambi Group plc. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
+I’m very interested in discussing the possibility of employment at skillja. 
+I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). 
+In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
 
-I like how you do this. I think that is good. I also like small detail on blog post. I thought small detail was neet.
+I believe I would be a great fit for skillja because skillja needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project, I took ownership of developing the entire backend in Node, deploying both the backend and frontend, integrating the entire Api, developing several components with React and Redux, to developing the UI. I’m also a collaborator; I communicate effectively and efficiently, and, while I am confident in my abilities, I can recognize when I need to take a step back to ask for assistance from a senior developer. In addition, my experience has allowed me to prioritize my problem solving and critical thinking skills as I overcame increasingly complicated challenges.
 
-I believe I would be a great fit for Kambi Group plc because Kambi Group plc needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project, I took ownership of developing the entire backend in Node, deploying both the backend and frontend, integrating the entire Api, developing several components with React and Redux, to developing the UI. I’m also a collaborator; I communicate effectively and efficiently, and, while I am confident in my abilities, I can recognize when I need to take a step back to ask for assistance from a senior developer. In addition, my experience has allowed me to prioritize my problem solving and critical thinking skills as I overcame increasingly complicated challenges.
-
-I would love to speak further about possible employment at Kambi Group plc sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
+I would love to speak further about possible employment at skillja sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
 Regards,
 
 Marshall Lanners
