65f021949dd6231b9dfae6dac31a9c6cde216eb7
lanners-marshall <lanners.marshall@gmail.com>
2019-07-29 12:56:52 -0700

    update

--- cover.js
+++ cover.js
@@ -5,13 +5,13 @@ Marshall Lanners
 lanners.marshall@gmail.com
 mlanners.com
 
-Dear SR Education Group Hiring Manager,
+Dear Promet Source Hiring Manager,
 
-I’m very interested in discussing the possibility of employment at SR Education Group. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
+I’m very interested in discussing the possibility of employment at Promet Source. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
 
-I believe I would be a great fit for SR Education Group because SR Education Group needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project, I took ownership of developing the entire backend in Node, deploying both the backend and frontend, integrating the entire Api, developing several components with React and Redux, to developing the UI. I’m also a collaborator; I communicate effectively and efficiently, and, while I am confident in my abilities, I can recognize when I need to take a step back to ask for assistance from a senior developer. In addition, my experience has allowed me to prioritize my problem solving and critical thinking skills as I overcame increasingly complicated challenges.
+I believe I would be a great fit for Promet Source because Promet Source needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project, I took ownership of developing the entire backend in Node, deploying both the backend and frontend, integrating the entire Api, developing several components with React and Redux, to developing the UI. I’m also a collaborator; I communicate effectively and efficiently, and, while I am confident in my abilities, I can recognize when I need to take a step back to ask for assistance from a senior developer. In addition, my experience has allowed me to prioritize my problem solving and critical thinking skills as I overcame increasingly complicated challenges.
 
-I would love to speak further about possible employment at SR Education Group sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
+I would love to speak further about possible employment at Promet Source sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
 Regards,
 
 Marshall Lanners
