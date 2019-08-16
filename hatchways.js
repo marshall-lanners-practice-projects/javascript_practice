73360d869487d6a241c0441fe7d5736b5ce8192c
lanners-marshall <lanners.marshall@gmail.com>
2019-08-15 17:27:06 -0700

    update

--- hatchways.js
+++ hatchways.js
@@ -1235,5 +1235,60 @@ var isCousins = function(root, x, y) {
   
   return parents[0] !== parents[1] && depths[0] === depths[1];
 };
+/*
+  Given a binary search tree, rearrange the tree in in-order so that the 
+  leftmost node in the tree is now the root of the tree, and every node has no 
+  left child and only 1 right child.
+
+  Example 1:
+  Input: [5,3,6,2,4,null,8,1,null,null,null,7,9]
+
+         5
+        / \
+      3    6
+     / \    \
+    2   4    8
+   /        / \ 
+  1        7   9
+
+  Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+              \
+               7
+                \
+                 8
+                  \
+                   9
+*/
+
+
+var increasingBST = function(root) {
+    const stack = []
+    const addStack = (root) => {
+        if (!root) return stack
+        if (root.left) addStack(root.left)
+        stack.push(root.val)
+        if (root.right) addStack(root.right)
+    }
+    addStack(root)
+    let treeNode = new TreeNode(0)
+    let current = treeNode
+    for (let i = 0; i < stack.length; i++){
+        current.right = new TreeNode(stack[i])
+        current = current.right
+    }
+    return treeNode.right
+};
 
 
