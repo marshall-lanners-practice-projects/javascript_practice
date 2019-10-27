dd688e20fc0ce672eef4bdfd14995bbaf6e55b38
lanners-marshall <lanners.marshall@gmail.com>
2019-10-27 11:40:15 -0700

    update

--- cover.js
+++ cover.js
@@ -1,18 +1,17 @@
 /*
 
-
 Marshall Lanners
 (206) 475 - 7021
 lanners.marshall@gmail.com
 mlanners.com
 
-Dear Agilysys, Inc. Hiring Manager,
+Dear Stripe Hiring Manager,
 
-I’m very interested in discussing the possibility of employment at Agilysys, Inc.. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
+I’m very interested in discussing the possibility of employment at Stripe. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
 
-I believe I would be a great fit for Agilysys, Inc. because Agilysys, Inc. needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project, I took ownership of developing the entire backend in Node, deploying both the backend and frontend, integrating the entire Api, developing several components with React and Redux, to developing the UI. I’m also a collaborator; I communicate effectively and efficiently, and, while I am confident in my abilities, I can recognize when I need to take a step back to ask for assistance from a senior developer. In addition, my experience has allowed me to prioritize my problem solving and critical thinking skills as I overcame increasingly complicated challenges.
+I believe I would be a great fit for Stripe because Stripe needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project worked on a full stack application written in Python Django for backend and react frontend. I took ownership of developing several components in React, integrating Redux, integrating the backend endpoints with the frontend, working with designers to implement the UI, and collaborating with engineers on the frontend and backend to make sure we met the customers expectations. 
 
-I would love to speak further about possible employment at Agilysys, Inc. sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
+I would love to speak further about possible employment at Stripe sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
 Regards,
 
 Marshall Lanners
