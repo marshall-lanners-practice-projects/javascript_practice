682502cbc066e0217521bc2137739a62d027a91f
lanners-marshall <lanners.marshall@gmail.com>
2019-11-13 02:19:35 -0800

    update

--- cover.js
+++ cover.js
@@ -1,21 +1,23 @@
 /*
-Barn2door
 
 Marshall Lanners
 (206) 475 - 7021
 lanners.marshall@gmail.com
 mlanners.com
+Dear Skytap Hiring Manager,
 
-Dear Barn2door Hiring Manager,
+I’m very interested in discussing the possibility of employment at Skytap. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript) and backend (Node, Express). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
 
-I’m very interested in discussing the possibility of employment at Barn2door. I’m a Full-Stack Web Developer with a focus on front-end technologies (React, JavaScript). In addition, I have over 6 years of customer service and 2 years of office management  experience. I am also an excellent problem solver and dedicated optimizer.
+I find it really amazing how you have optimized the cloud experience. I know first how complicated it is to integrate some cloud services. I loved your story on how you pioneered the path for IBM Power Systems to the cloud and how you make the public cloud compatible with traditional applications that are anchored in the data center. I know that working at such an innovative company would be a really amazing experience. 
 
-I believe I would be a great fit for Barn2door because Barn2door needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project worked on a full stack application written in Python Django for backend and react frontend. I took ownership of developing several components in React, integrating Redux, integrating the backend endpoints with the frontend, working with designers to implement the UI, and collaborating with engineers on the frontend and backend to make sure we met the customers expectations. 
+I believe I would be a great fit for Skytap because Skytap needs people who deeply care about quality, who are curious to learn all aspects of the tech stack, and who are willing to take ownership of their codebase. My current web development experience, along with my previous experience in customer service and office management, makes me uniquely qualified. In my most recent project worked on a full stack application written in Node Django for backend and react frontend. I took ownership of developing several components in React, integrating Redux, integrating the backend endpoints with the frontend, working with designers to implement the UI, and collaborating with engineers on the frontend and backend to make sure we met the customers expectations. 
 
-I would love to speak further about possible employment at Barn2door sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
+I would love to speak further about possible employment at Skytap sometime this week. If you’re interested, or have any further questions, feel free to reach out by phone or email. Thank you for your time and I look forward to speaking with you soon!
 Regards,
 
 Marshall Lanners
+
+
 */
 
 console.log(this)
